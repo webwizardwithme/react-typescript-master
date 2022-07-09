acabd500e9bfea7c3b1119bb58808317dd09bb71
sergio1221 <sergio.pinilos.dev@gmail.com>
2022-07-09 04:28:45 -0500

    03-State

---
+++ src/nameEdit.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+interface Props {
+  userName: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+export const NameEditComponent: React.FC<Props> = (props: Props) => {
+  return (
+    <>
+      <label>Update name:</label>
+      <input type="text" value={props.userName} onChange={props.onChange} />
+    </>
+  );
+};
